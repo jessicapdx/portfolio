021530aa7486cc6f8cb9ea7285ff72827374424b
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2023-03-25 23:32:45 -0700

    added portfolio content and contact info

--- src/Contact.js
+++ src/Contact.js
@@ -1,4 +1,5 @@
 import "./styling/Contact.css";
+import { Link } from "react-router-dom";
 
 export default function Contact() {
   return (
@@ -6,8 +7,16 @@ export default function Contact() {
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Contact Me</h1>
-          <p className="mb-5">Contact me body text</p>
-          <button className="btn btn-primary">LinkedIn</button>
+          <p className="mb-5">
+            Feel free to connect with me on LinkedIn or email me at
+            <br />
+            <a href="mailto:hello@jessicabutler.io">hello@jessicabutler.io</a>
+          </p>
+          <button className="rounded-full btn btn-primary">
+            <Link to="https://www.linkedin.com/in/jessicabbutler/">
+              LinkedIn
+            </Link>
+          </button>
         </div>
       </div>
     </div>
--- src/Portfolio.js
+++ src/Portfolio.js
@@ -1,13 +1,49 @@
 import "./styling/Portfolio.css";
+import dictionaryApp from "./img/dictionaryapp.png";
+import weatherApp from "./img/weatherapp.png";
 
 export default function Portfolio() {
   return (
     <div className="hero min-h-screen">
-      <div className="hero-content text-center text-neutral-content">
+      <div className="hero-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Portfolio</h1>
-          <div className="columns-3"></div>
-          <button className="btn btn-ghost">Contact Me</button>
+          <div className="p-5">
+            <a
+              href="https://jb-shecodes-final-project.netlify.app/"
+              alt="Link to live Weather application website"
+            >
+              Weather App Live
+            </a>
+            <img
+              src={weatherApp}
+              alt="Screenshot of weather application"
+              className="p-3"
+            ></img>
+            <a
+              href="https://github.com/jessicapdx/react-weather-v2"
+              alt="Weather App built with React, JS, Bootstrap"
+            >
+              Source Code
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://jb-react-dictionary-app.netlify.app/"
+              alt="Link to live Dictionary app website"
+            >
+              Dictionary App Live
+            </a>
+            <br />
+            <img
+              src={dictionaryApp}
+              alt="Dictionary application screenshot"
+              className="p-5"
+            ></img>
+            <a href="https://github.com/jessicapdx/dictionary-project">
+              Source Code
+            </a>
+          </div>
         </div>
       </div>
     </div>
