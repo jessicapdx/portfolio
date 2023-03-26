f6d51dd39e93de3b155f3a1e4a7b94fc3be774d0
Jessica Barrows-Butler <jessica.b.barrows@gmail.com>
2023-03-26 13:27:59 -0700

    updated contact page content

--- src/Contact.js
+++ src/Contact.js
@@ -3,14 +3,16 @@ import { Link } from "react-router-dom";
 
 export default function Contact() {
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Contact Me</h1>
+    <div className="min-h-screen">
+      <div className="text-center justify-content-center grid grid-rows-2">
+        <h1 className="mb-5 text-4xl font-bold">Contact Me</h1>
+        <div className="bg-white opacity-75 rounded-lg p-3 ">
           <p className="mb-5">
             Feel free to connect with me on LinkedIn or email me at
             <br />
-            <a href="mailto:hello@jessicabutler.io">hello@jessicabutler.io</a>
+            <a href="mailto:hello@jessicabutler.io" className="font-bold">
+              💌 hello@jessicabutler.io 💌
+            </a>
           </p>
           <button className="rounded-full btn btn-primary">
             <Link to="https://www.linkedin.com/in/jessicabbutler/">
